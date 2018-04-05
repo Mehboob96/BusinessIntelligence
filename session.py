db0a78b769e95c4e661d13318258862cc4dd37c9
Mehboob96 <smehboobs62@gmail.com>
2018-04-06 00:54:56 +0530

    second presentation

--- session.py
+++ session.py
@@ -1,9 +1,14 @@
 import pandas as pd
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
-from flask import Flask, session, render_template, request, redirect, g, url_for
+import pprint
+from flask import Flask, session, render_template, request, redirect, g, url_for,jsonify
 from database import *
 import os
+from bson.json_util import dumps
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.example
 
 app =Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -22,17 +27,77 @@ def index():
 
 @app.route('/home')
 def protected():
-    if g.user:
+    if getsession():
+        db.balancesheet.find({"business_id":session['user']},{"assets.total":1})
         return render_template('index.html',user = session['user'])
 
     return redirect(url_for('index'))
 
+#add new statements GUI
 @app.route('/add')
 def add():
-    if g.user:
+    if getsession():
         return render_template('plain_page.html',user = session['user'])
 
     return redirect(url_for('protected'))
+
+#add new statements in DB
+@app.route('/addStatement',methods=['POST'])
+def addStatement():
+    if getsession():
+        result = request.form.getlist('assettype')
+        result2 = request.form.getlist('assetamount')
+        result3 = request.form.getlist('liabilitytype')
+        result4 = request.form.getlist('liabilityamount')
+        totalasset = 0
+        totalliability = 0
+
+        assets = {}
+        for i in range(len(result)-1):
+            assets[result[i]] = result2[i]
+            totalasset = totalasset + int(result2[i])
+
+        liabilities = {}
+        for i in range(len(result3)-1):
+            liabilities[result3[i]] = result4[i]
+            totalliability = totalliability + int(result4[i])
+
+        doc = {
+            "business_id" : session['user'],
+            "year" : request.form.get('year',None),
+            "month" : request.form.get('month',None),
+            "assets":assets,
+            "totalasset" : totalasset,
+            "liabilities":liabilities,
+            "totalliability": totalliability
+        }
+        # return jsonify(doc);
+
+        db.balancesheet.insert(doc)
+        # print "ASSETS"
+        # for i in range(len(result)-1):
+        #     print result[i]," : ",result2[i]
+        #     totalasset = totalasset + int(result2[i])
+        # print totalasset
+        # print "LIABILITY"
+        # for i in range(len(result3)-1):
+        #     print result3[i]," : ",result4[i]
+        #     totalliability = totalliability + int(result4[i])
+        # print totalliability
+        # f = request.form
+        # for key in f.keys():
+            # for value in f.getlist(key):
+                # print key,":",value
+        return "Success"
+        # pprint.pprint(getsession())
+
+    return redirect(url_for('index'))
+
+
+@app.route('/getBalanceSheet',methods=['POST'])
+def getBalanceSheet():
+    bs = db.balancesheet.find({'business_id':'Mehboob'})
+    return dumps(bs)
 
 @app.before_request
 def before_request():
@@ -43,9 +108,9 @@ def before_request():
 @app.route('/getsession')
 def getsession():
     if 'user' in session:
-        return session['user']
+        return True
 
-    return 'You are not Logged in!'
+    return False
 
 @app.route('/dropsession')
 def dropsession():
